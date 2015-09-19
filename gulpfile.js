115b91ba0b5f79a0101ade39a9c7568318c3c335
China Wang <chwang@thoughtworks.com>
2015-09-18 20:48:40 -0700

    added more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,29 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
+var shell = require('gulp-shell');
 
-gulp.task('browserify', function () {
+gulp.task('js', function () {
     browserify('./src/js/app.js')
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('./public'));
 });
+
+gulp.task('css', function () {
+    gulp.src('./src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('build', ['js', 'css']);
+
+gulp.task('server', shell.task('node server.js'));
+
+gulp.task('watch', function () {
+    gulp.watch('./src/scss/*.scss', ['css']);
+    gulp.watch('./src/js/*.js', ['js']);
+});
+
+gulp.task('default', ['build', 'server', 'watch']);
